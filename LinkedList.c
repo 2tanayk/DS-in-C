850cfe3819325f5b8959e0917b5c0e75568d76ed
Tanay Kamath <2tanaykamath@gmail.com>
2020-09-04 23:18:56 +0530

    added user i/p and documentation

--- LinkedList.c
+++ LinkedList.c
@@ -1,42 +1,97 @@
+/**
+ * Author:Tanay Kamath
+ */
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
 #include<string.h>
 #include<stdbool.h>
-
 struct node
 {
+    //self-referential structure to create a node of the linked list
     int data;
     struct node *address;
 };
-
+//function prototypes
 struct node *add_at_end(struct node*,int);
 void traverse(struct node*);
 struct node* insert(struct node*,int,int);
 struct node* delete_element(struct node*,int);
 
+//main function
 void main()
 {
+    //head of the linked list
     struct node *head=(struct node*)malloc(sizeof(struct node));
     head->data=10;
     head->address=NULL;
 
     struct node *current=head;
 
+    bool flag=true;
+
+    //adding elements to the linked list in O(1) time complexity using current pointer
     current=add_at_end(current,34);
     current=add_at_end(current,12);
     current=add_at_end(current,67);
     current=add_at_end(current,100);
+    do
+    {
 
+        int ch;
+        printf("Enter your choice:\n1)add\n2)insert\n3)delete\n4)display\n5)exit\n");
+        scanf("%d",&ch);
 
-    traverse(head);
-    insert(head,23,3);
+        int e,pos;
+        switch(ch)
+        {
+        case 1:
+            printf("Enter the element you want to add:");
+            scanf("%d",&e);
+            current=add_at_end(current,e);
+            break;
+        case 2:
+            printf("Enter the element you want to insert and its position:");
+            scanf("%d%d",&e,&pos);
+            if(pos==1)
+            {
+                head=insert(head,e,pos);
+            }
+            else
+            {
+                insert(head,e,pos);
+            }
+            break;
+        case 3:
+            printf("Enter the position of the element you want to delete:");
+            scanf("%d",&pos);
+            if(pos==1)
+            {
+                head=delete_element(head,pos);
+            }
+            else
+            {
+                delete_element(head,pos);
+            }
+            break;
+        case 4:
+            traverse(head);
+            break;
+        case 5:
+            flag=false;
+            break;
+        default:
+            printf("Invalid choice");
+        }
+    }
+    while(flag);
 
+    // traverse(head);
+    //insert(head,23,3);
+    //delete_element(head,4);
     //head=delete_element(head,4);
-
-    traverse(head);
-
-
+    //traverse(head);
 }
 
 struct node* add_at_end(struct node* current,int data)
@@ -62,7 +117,7 @@ void traverse(struct node* temp)
 
 struct node* insert(struct node* head,int data,int pos)
 {
-    struct node *ptr=head;
+    struct node *ptr=head; //pointer for traversing the linked list
 
     struct node *new_node=(struct node*)malloc(sizeof(struct node));
     new_node->data=data;
@@ -71,7 +126,7 @@ struct node* insert(struct node* head,int data,int pos)
     {
         new_node->address=head;
         head=new_node;
-        return head;
+        return head;  //we return pointer head so that it points to the new first element
     }
     else
     {
@@ -83,9 +138,9 @@ struct node* insert(struct node* head,int data,int pos)
             pos--;
         }
 
-        new_node->address=ptr->address;
-        ptr->address=new_node;
-        return;
+        new_node->address=ptr->address;//initializing address of inserted node to that of its next node
+        ptr->address=new_node;//re-initializing address of the previous node to that of the new node
+        return;//we don't have anything to return hence we return none
     }
 }
 struct node* delete_element(struct node* head,int pos)
@@ -97,15 +152,14 @@ struct node* delete_element(struct node* head,int pos)
     {
         printf("List is empty!");
         return;
-
     }
-    else if(pos==1)
+    else if(pos==1) //checking if linked list contains only one element
     {
         head=head->address;
-        free(current);
-        current=NULL;
+        free(current);//we need to free the heap memory allocated by us
+        current=NULL;//to avoid a dangling pointer we assign NULL to it
 
-        return head;
+        return head;//we return pointer head so that it points to the new first element
     }
     else
     {
@@ -116,9 +170,9 @@ struct node* delete_element(struct node* head,int pos)
             pos--;
         }
         previous->address=current->address;
-        free(current);
-        current=NULL;
-        return;
+        free(current);//we need to free the heap memory allocated by us
+        current=NULL;//to avoid a dangling pointer we assign NULL to it
+        return;//we don't have anything to return hence we return none
     }
 }
 
